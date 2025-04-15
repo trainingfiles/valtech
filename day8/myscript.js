7c50cecd99f7c7ccc8b304f1d1f9228fa8b2d6ad
Training Repo <70316959+trainingfiles@users.noreply.github.com>
2025-04-15 17:37:59 +0530

    Add files via upload

---
+++ day8/myscript.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("button")[0].onclick = function () {
+  alert("you clicked the add button");
+};
+document.getElementsByTagName("button")[1].onclick = function () {
+  alert("you clicked the remove button");
+};
